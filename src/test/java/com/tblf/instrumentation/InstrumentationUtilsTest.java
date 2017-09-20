d68e24a5b1eb221d682964fe22e09dbced688bd0
THIBAULT BEZIERS LA FOSSE <thiblf@gmail.com>
2017-09-20 17:29:23 +0200

    Added a test class verifying that the InstrumentationUtils class is able to fetch a compiled file, from its bin folder and qualified name

---
+++ src/test/java/com/tblf/instrumentation/InstrumentationUtilsTest.java
@@ -0,0 +1,24 @@
+package com.tblf.instrumentation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Thibault on 20/09/2017.
+ */
+public class InstrumentationUtilsTest {
+
+    @Test
+    public void checkGetClassFile() {
+        File folder = new File("src/test/resources/binaries/junit/bin");
+        try {
+            File classFile = InstrumentationUtils.getClassFile(folder, "org.junit.experimental.categories.AllCategoriesTests");
+            Assert.assertTrue(classFile.exists());
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+}
