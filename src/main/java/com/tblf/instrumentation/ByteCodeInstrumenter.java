2444635e54ed1672bb4327ea84b7902caaa5b11d
THIBAULT BEZIERS LA FOSSE <thiblf@gmail.com>
2017-09-20 13:56:56 +0200

    Instrumentation interface and empty class added. The instrumentation has to be completed right before the classloading step

---
+++ src/main/java/com/tblf/instrumentation/ByteCodeInstrumenter.java
@@ -0,0 +1,15 @@
+package com.tblf.instrumentation;
+import java.util.List;
+
+/**
+ * Created by Thibault on 20/09/2017.
+ */
+public class ByteCodeInstrumenter implements Instrumenter {
+
+    @Override
+    public void instrument(List<String> targets, List<String> tests) {
+
+
+    }
+
+}
