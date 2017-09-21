6e50ff180da2b41881be21d61bb916b08e40063b
THIBAULT BEZIERS LA FOSSE <thiblf@gmail.com>
2017-09-21 17:07:22 +0200

    Main method added, plus a test case instrumenting and running the entire junit test suite in order to compute the analysis*

---
+++ src/test/java/com/tblf/main/Apptest.java
@@ -0,0 +1,19 @@
+package com.tblf.main;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Thibault on 21/09/2017.
+ */
+public class Apptest {
+
+    @Test
+    public void checkMain() {
+        try {
+            App.main(new String[]{"src/test/resources/junit_java.xmi", "src/test/resources/binaries/junit/bin"});
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+}
