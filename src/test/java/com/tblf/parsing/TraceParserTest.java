144a22bb13deb5ee73b726109fb1f6855e4d40e0
orichalque <thiblf@gmail.com>
2017-10-04 15:06:58 +0200

    Added a test method verifying the parsing

--- src/test/java/com/tblf/parsing/TraceParserTest.java
+++ src/test/java/com/tblf/parsing/TraceParserTest.java
@@ -5,21 +5,26 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.gmt.modisco.java.ClassDeclaration;
-import org.eclipse.gmt.modisco.java.CompilationUnit;
-import org.eclipse.gmt.modisco.java.MethodDeclaration;
+import org.eclipse.gmt.modisco.java.*;
 import org.eclipse.gmt.modisco.java.Package;
 import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.eclipse.gmt.modisco.java.emf.JavaPackage;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.KdmPackage;
 import org.eclipse.modisco.java.composition.javaapplication.*;
+import org.eclipse.modisco.kdm.source.extension.ASTNodeSourceRegion;
+import org.eclipse.modisco.kdm.source.extension.CodeUnit2File;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Created by Thibault on 02/10/2017.
@@ -30,6 +35,13 @@ public class TraceParserTest {
 
     @Before
     public void setUp() throws IOException {
+
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(Level.FINE);
+        for (Handler h : rootLogger.getHandlers()) {
+            h.setLevel(Level.FINE);
+        }
+
         JavaapplicationPackage.eINSTANCE.eClass();
         JavaPackage.eINSTANCE.eClass();
         KdmPackage.eINSTANCE.eClass();
@@ -60,9 +72,36 @@ public class TraceParserTest {
         MethodDeclaration myMethod = JavaFactory.eINSTANCE.createMethodDeclaration();
         myMethod.setName("myMethod");
 
+        Statement statement1 = JavaFactory.eINSTANCE.createExpressionStatement();
+        Statement statement2 = JavaFactory.eINSTANCE.createExpressionStatement();
+
         test.getBodyDeclarations().add(init);
         test.getBodyDeclarations().add(myTest);
         target.getBodyDeclarations().add(myMethod);
+
+        ASTNodeSourceRegion testMethodNode = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
+        testMethodNode.setStartLine(4);
+        testMethodNode.setEndLine(6);
+        testMethodNode.setNode(myTest);
+
+        ASTNodeSourceRegion targetMethodNode = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
+        targetMethodNode.setStartLine(4);
+        targetMethodNode.setEndLine(7);
+        targetMethodNode.setNode(myMethod);
+
+        ASTNodeSourceRegion statementNode1 = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
+        statementNode1.setStartLine(5);
+        statementNode1.setEndLine(5);
+        statementNode1.setStartPosition(12);
+        statementNode1.setEndPosition(65);
+        statementNode1.setNode(statement1);
+
+        ASTNodeSourceRegion statementNode2 = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
+        statementNode2.setStartLine(5);
+        statementNode2.setEndLine(5);
+        statementNode2.setStartPosition(12);
+        statementNode2.setEndPosition(65);
+        statementNode2.setNode(statement2);
 
         Java2Directory java2Directory = JavaapplicationFactory.eINSTANCE.createJava2Directory();
         java2Directory.setJavaPackage(aPackage);
@@ -71,11 +110,13 @@ public class TraceParserTest {
         CompilationUnit testUnit = JavaFactory.eINSTANCE.createCompilationUnit();
         testUnit.setName("Test.java");
         testFile.setUnit(testUnit);
+        testFile.getChildren().addAll(List.of(testMethodNode));
 
         Java2File targetFile = JavaapplicationFactory.eINSTANCE.createJava2File();
         CompilationUnit targetUnit = JavaFactory.eINSTANCE.createCompilationUnit();
         targetUnit.setName("Target.java");
         targetFile.setUnit(targetUnit);
+        targetFile.getChildren().addAll(List.of(targetMethodNode, statementNode1, statementNode2));
 
         java2Directory.getJava2FileChildren().add(testFile);
         java2Directory.getJava2FileChildren().add(targetFile);
@@ -87,7 +128,7 @@ public class TraceParserTest {
         resourceSet = new ResourceSetImpl();
         resourceSet.setResourceFactoryRegistry(reg);
 
-        Resource resource = resourceSet.createResource(URI.createURI("MyModel.xmi"));
+        Resource resource = resourceSet.createResource(URI.createURI("com.tblf.pk1_java2kdm.xmi"));
         resource.getContents().add(java2Directory);
     }
 
