97f453dac189ae3a2bacc706080440593777e2ae
orichalque <thiblf@gmail.com>
2017-10-05 15:50:17 +0200

    Removed all the set up method creating models from scatch, in order to use real-life modisco models created out of a small java project

--- src/test/java/com/tblf/parsing/TraceParserTest.java
+++ src/test/java/com/tblf/parsing/TraceParserTest.java
@@ -1,30 +1,42 @@
 package com.tblf.parsing;
 
+import com.tblf.Model.Model;
+import com.tblf.Model.ModelFactory;
+import com.tblf.Model.ModelPackage;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.gmt.modisco.java.*;
 import org.eclipse.gmt.modisco.java.Package;
 import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.eclipse.gmt.modisco.java.emf.JavaPackage;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.KdmPackage;
+import org.eclipse.gmt.modisco.omg.kdm.source.SourcePackage;
 import org.eclipse.modisco.java.composition.javaapplication.*;
 import org.eclipse.modisco.kdm.source.extension.ASTNodeSourceRegion;
 import org.eclipse.modisco.kdm.source.extension.CodeUnit2File;
+import org.eclipse.modisco.kdm.source.extension.ExtensionPackage;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Created by Thibault on 02/10/2017.
@@ -34,22 +46,18 @@ public class TraceParserTest {
     private ResourceSet resourceSet;
 
     @Before
-    public void setUp() throws IOException {
-
+    public void setUpRealCode() throws IOException {
+        resourceSet = new ResourceSetImpl();
         Logger rootLogger = LogManager.getLogManager().getLogger("");
         rootLogger.setLevel(Level.FINE);
         for (Handler h : rootLogger.getHandlers()) {
             h.setLevel(Level.FINE);
         }
 
-        JavaapplicationPackage.eINSTANCE.eClass();
-        JavaPackage.eINSTANCE.eClass();
-        KdmPackage.eINSTANCE.eClass();
-
         StringBuilder sb = new StringBuilder();
-        sb.append("&:com.tblf.pk1.Test:<init>\n");
-        sb.append("&:com.tblf.pk1.Test:myTest\n");
-        sb.append("%:com.tblf.pk1.Target:myMethod\n");
+        sb.append("&:com.tblf.SimpleProject.AppTest:<init>\n");
+        sb.append("&:com.tblf.SimpleProject.AppTest:testApp\n");
+        sb.append("%:com.tblf.SimpleProject.App:method\n");
         sb.append("?:5\n");
         sb.append("?:6");
 
@@ -57,84 +65,51 @@ public class TraceParserTest {
 
         Files.write(trace.toPath(), sb.toString().getBytes());
 
-        Package aPackage = JavaFactory.eINSTANCE.createPackage();
-        aPackage.setName("com.tblf.pk1");
-
-        ClassDeclaration test = JavaFactory.eINSTANCE.createClassDeclaration();
-        test.setName("Test");
-        ClassDeclaration target = JavaFactory.eINSTANCE.createClassDeclaration();
-        target.setName("Target");
-
-        MethodDeclaration init = JavaFactory.eINSTANCE.createMethodDeclaration();
-        init.setName("<init>");
-        MethodDeclaration myTest = JavaFactory.eINSTANCE.createMethodDeclaration();
-        myTest.setName("myTest");
-        MethodDeclaration myMethod = JavaFactory.eINSTANCE.createMethodDeclaration();
-        myMethod.setName("myMethod");
-
-        Statement statement1 = JavaFactory.eINSTANCE.createExpressionStatement();
-        Statement statement2 = JavaFactory.eINSTANCE.createExpressionStatement();
-
-        test.getBodyDeclarations().add(init);
-        test.getBodyDeclarations().add(myTest);
-        target.getBodyDeclarations().add(myMethod);
-
-        ASTNodeSourceRegion testMethodNode = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
-        testMethodNode.setStartLine(4);
-        testMethodNode.setEndLine(6);
-        testMethodNode.setNode(myTest);
-
-        ASTNodeSourceRegion targetMethodNode = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
-        targetMethodNode.setStartLine(4);
-        targetMethodNode.setEndLine(7);
-        targetMethodNode.setNode(myMethod);
-
-        ASTNodeSourceRegion statementNode1 = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
-        statementNode1.setStartLine(5);
-        statementNode1.setEndLine(5);
-        statementNode1.setStartPosition(12);
-        statementNode1.setEndPosition(65);
-        statementNode1.setNode(statement1);
-
-        ASTNodeSourceRegion statementNode2 = JavaapplicationFactory.eINSTANCE.createJavaNodeSourceRegion();
-        statementNode2.setStartLine(5);
-        statementNode2.setEndLine(5);
-        statementNode2.setStartPosition(12);
-        statementNode2.setEndPosition(65);
-        statementNode2.setNode(statement2);
-
-        Java2Directory java2Directory = JavaapplicationFactory.eINSTANCE.createJava2Directory();
-        java2Directory.setJavaPackage(aPackage);
-
-        Java2File testFile = JavaapplicationFactory.eINSTANCE.createJava2File();
-        CompilationUnit testUnit = JavaFactory.eINSTANCE.createCompilationUnit();
-        testUnit.setName("Test.java");
-        testFile.setUnit(testUnit);
-        testFile.getChildren().addAll(List.of(testMethodNode));
-
-        Java2File targetFile = JavaapplicationFactory.eINSTANCE.createJava2File();
-        CompilationUnit targetUnit = JavaFactory.eINSTANCE.createCompilationUnit();
-        targetUnit.setName("Target.java");
-        targetFile.setUnit(targetUnit);
-        targetFile.getChildren().addAll(List.of(targetMethodNode, statementNode1, statementNode2));
-
-        java2Directory.getJava2FileChildren().add(testFile);
-        java2Directory.getJava2FileChildren().add(targetFile);
+        EPackage.Registry.INSTANCE.put(JavaPackage.eNS_URI, JavaPackage.eINSTANCE);
+        EPackage.Registry.INSTANCE.put(JavaapplicationPackage.eNS_URI, JavaapplicationPackage.eINSTANCE);
+        EPackage.Registry.INSTANCE.put(KdmPackage.eNS_URI, KdmPackage.eINSTANCE);
+        EPackage.Registry.INSTANCE.put(SourcePackage.eNS_URI, SourcePackage.eINSTANCE);
+        EPackage.Registry.INSTANCE.put(ModelPackage.eNS_URI, ModelPackage.eINSTANCE);
 
         Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
         Map<String, Object> m = reg.getExtensionToFactoryMap();
         m.put("xmi", new XMIResourceFactoryImpl());
 
-        resourceSet = new ResourceSetImpl();
-        resourceSet.setResourceFactoryRegistry(reg);
+        File file = new File("src/test/resources/models/simpleProject");
+        Files.walk(file.toPath()).filter(path -> path.toString().endsWith(".xmi")).forEach(path -> {
 
-        Resource resource = resourceSet.createResource(URI.createURI("com.tblf.pk1_java2kdm.xmi"));
-        resource.getContents().add(java2Directory);
+            System.out.println("now adding: "+path.getFileName().toString());
+            try {
+                Resource resource = resourceSet.getResource(URI.createURI(path.toUri().toURL().toString()), true);
+                System.out.println(resource);
+                resourceSet.getResources().add(resource);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        Assert.assertTrue(resourceSet.getResources().stream().filter(resource -> resource.getContents().get(0) == null).collect(Collectors.toList()).isEmpty());
     }
 
     @Test
     public void checkParse() throws IOException {
         TraceParser traceParser = new TraceParser(trace, resourceSet);
-        traceParser.parse();
+        Model model = traceParser.parse();
+
+        File file = new File("src/test/resources/myAnalysisModel.xmi");
+
+        if (file.exists()) {
+            file.delete();
+        }
+
+        file.createNewFile();
+        Resource resource = resourceSet.createResource(URI.createURI(file.toURI().toURL().toString()));
+
+        resource.getContents().add(model);
+
+        model.getAnalyses().forEach(System.out::println);
+        resource.save(Collections.EMPTY_MAP);
     }
+
 }
