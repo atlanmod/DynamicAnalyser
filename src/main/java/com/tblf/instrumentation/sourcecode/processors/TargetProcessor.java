267737409fb55b2a034ac8176110f98c0054c718
orichalque <thiblf@gmail.com>
2017-10-06 13:50:40 +0200

    Added the processor class used to instrument the SUT

---
+++ src/main/java/com/tblf/instrumentation/sourcecode/processors/TargetProcessor.java
@@ -0,0 +1,11 @@
+package com.tblf.instrumentation.sourcecode.processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtElement;
+
+public class TargetProcessor extends AbstractProcessor {
+    @Override
+    public void process(CtElement ctElement) {
+
+    }
+}
