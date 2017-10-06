fb39cf3c0e4b4c53c5b62d91d35a7550d838e29d
orichalque <thiblf@gmail.com>
2017-10-06 13:50:57 +0200

    Added the processor class used to instrument the test classes

---
+++ src/main/java/com/tblf/instrumentation/sourcecode/processors/TestProcessor.java
@@ -0,0 +1,11 @@
+package com.tblf.instrumentation.sourcecode.processors;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtElement;
+
+public class TestProcessor extends AbstractProcessor {
+    @Override
+    public void process(CtElement ctElement) {
+
+    }
+}
