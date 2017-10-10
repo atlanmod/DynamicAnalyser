5607543def890c401c3bb234cdbbbcaa10422ce0
orichalque <thiblf@gmail.com>
2017-10-10 13:11:35 +0200

    Processors updated. Now adding the right statements inside the source code. Might need some tests on a real project though.

--- src/main/java/com/tblf/instrumentation/sourcecode/processors/TargetProcessor.java
+++ src/main/java/com/tblf/instrumentation/sourcecode/processors/TargetProcessor.java
@@ -4,23 +4,41 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCodeSnippetStatement;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtType;
+
+import java.util.Collection;
 
 public class TargetProcessor extends AbstractProcessor<CtStatement> {
+
+    private Collection targets;
+
+    public TargetProcessor(Collection<String> targets) {
+        super();
+        this.targets = targets;
+    }
+
     @Override
     public void process(CtStatement ctStatement) {
+        CtClass ctType = ctStatement.getParent(CtClass.class);
 
-            if (!ctStatement.isImplicit() && ! (ctStatement instanceof CtBlock) && !(ctStatement instanceof CtCodeSnippetStatement)) {
-                //The ctStatement is not a super(). we can insert an other just before it
-                String value = String.format("\"%s - %s\"", ctStatement.getPosition().getSourceStart(), ctStatement.getPosition().getSourceEnd());
-                String toAdd = "System.out.println(" + value + ")";
-                CtCodeSnippetStatement ctCodeSnippetStatement = getFactory().Core().createCodeSnippetStatement();
-                ctCodeSnippetStatement.setValue(toAdd);
-                try {
-                    ctStatement.insertBefore(ctCodeSnippetStatement);
-                } catch (Exception e) {
-                    System.out.println("Couldn't instrument statement: "+ctStatement);
-                    e.printStackTrace();
-                }
-            }
+        //Is a target
+        if (ctType != null && targets.contains(ctType.getQualifiedName())) {
+            instrumentStatement(ctStatement);
+        }
+    }
+
+    /**
+     * Insert a Statement before each existing statement, in order to trace the execution of all the statements of the code during the execution
+     * @param ctStatement
+     */
+    private void instrumentStatement(CtStatement ctStatement) {
+        if (!ctStatement.isImplicit() && !(ctStatement instanceof CtBlock) && !(ctStatement instanceof CtCodeSnippetStatement)) {
+            //The ctStatement is not a super(). we can insert an other just before it
+            CtCodeSnippetStatement ctCodeSnippetStatement = getFactory().Core().createCodeSnippetStatement();
+            // source end +1 to match MoDisco
+            ctCodeSnippetStatement.setValue(String.format("com.tblf.Link.Calls.match(\"%s\", \"%s\")", ctStatement.getPosition().getSourceStart(), ctStatement.getPosition().getSourceEnd()+1));
+            ctStatement.insertBefore(ctCodeSnippetStatement);
+        }
     }
 }
--- src/main/java/com/tblf/instrumentation/sourcecode/processors/TestProcessor.java
+++ src/main/java/com/tblf/instrumentation/sourcecode/processors/TestProcessor.java
@@ -3,23 +3,68 @@ package com.tblf.instrumentation.sourcecode.processors;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCodeSnippetStatement;
-import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtType;
 
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+
 public class TestProcessor extends AbstractProcessor<CtType<?>> {
+    private static final Logger LOGGER = Logger.getLogger("Processor");
+    private Collection<String> tests;
+
+    /**
+     * Constructor
+     * @param tests
+     */
+    public TestProcessor(Collection<String> tests) {
+        super();
+        this.tests = tests;
+    }
 
     @Override
     public void process(CtType<?> ctType) {
+
+        if (tests.contains(ctType.getQualifiedName())) {
+            //test behaviour
+            instrumentTest(ctType);
+        } else {
+            //target behaviour
+            instrumentTarget(ctType);
+        }
+    }
+
+    /**
+     * Instrument a test class by adding a method call at the beginning of every method having a body
+     * @param ctType
+     */
+    private void instrumentTest(CtType<?> ctType) {
+        LOGGER.fine("Instrumenting the test class: "+ctType.getQualifiedName());
         ctType.getMethods().forEach(ctMethod -> {
             CtBlock ctBlock = ctMethod.getBody();
             if (ctBlock != null) {
-                //The block exist, we're not instrumenting an interface
-                String value = String.format("\"%s - %s\"", ctMethod.getParent(CtClass.class).getQualifiedName(), ctMethod.getSimpleName());
-                String toAdd = "System.out.println("+value+")";
                 CtCodeSnippetStatement ctCodeSnippetStatement = getFactory().Core().createCodeSnippetStatement();
-                ctCodeSnippetStatement.setValue(toAdd);
+                ctCodeSnippetStatement.setValue(String.format("com.tblf.Link.Calls.setTestMethod(\"%s\", \"%s\")", ctType.getQualifiedName(), ctMethod.getSimpleName()));
                 ctBlock.insertBegin(ctCodeSnippetStatement);
             }
         });
     }
+
+    /**
+     * Add the right method at the beginning of the SUT method in order to build the trace during the execution
+     * @param ctType
+     */
+    private void instrumentTarget(CtType<?> ctType) {
+        LOGGER.fine("Instrumenting the SUT class: "+ctType.getQualifiedName());
+        ctType.getMethods().forEach(ctMethod -> {
+            CtBlock ctBlock = ctMethod.getBody();
+            if (ctBlock != null) {
+                CtCodeSnippetStatement ctCodeSnippetStatement = getFactory().Core().createCodeSnippetStatement();
+                ctCodeSnippetStatement.setValue(String.format("com.tblf.Link.Calls.setTargetMethod(\"%s\", \"%s\")", ctType.getQualifiedName(), ctMethod.getSimpleName()));
+                ctBlock.insertBegin(ctCodeSnippetStatement);
+            }
+        });
+    }
+
+
 }
