5af529ece61670d18aed50e291956c008889feae
orichalque <thiblf@gmail.com>
2017-10-17 12:14:53 +0200

    Completed the GitCaller, now testing on a real mvn project with a analysis model already computed

--- src/main/java/com/tblf/diff/GitCaller.java
+++ src/main/java/com/tblf/diff/GitCaller.java
@@ -1,23 +1,26 @@
 package com.tblf.diff;
 
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.Position;
+import com.github.javaparser.Range;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.Edit;
 import org.eclipse.jgit.diff.RawTextComparator;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.patch.FileHeader;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,15 +38,18 @@ public class GitCaller {
     private RevTree oldTree;
     private RevTree newTree;
 
+    private ResourceSet resourceSet;
+
     /**
      * Constructor initializing the {@link Git}
      *
      * @param pomFolder
      */
-    public GitCaller(File pomFolder) {
+    public GitCaller(File pomFolder, ResourceSet set) {
         try {
             Git git = Git.open(pomFolder);
             repository = git.getRepository();
+            resourceSet = set;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,6 +65,10 @@ public class GitCaller {
             ObjectId previous = repository.resolve(oldCommitID);
             ObjectId current = repository.resolve("HEAD");
 
+            if (previous == null || current == null) {
+                throw new IOException("Cannot resolve the commits");
+            }
+
             oldTree = new RevWalk(repository).parseCommit(previous).getTree();
             newTree = new RevWalk(repository).parseCommit(current).getTree();
             diffFormatter = new DiffFormatter(new LogOutputStream(LOGGER, Level.FINE));
@@ -73,6 +83,10 @@ public class GitCaller {
         }
     }
 
+    /**
+     * Iterate over a {@link DiffEntry} list and, get the impacted test cases out of it
+     * @param diffEntries
+     */
     private void analyseDiffs(List<DiffEntry> diffEntries) {
         diffEntries.forEach(diffEntry -> {
             try {
@@ -101,47 +115,45 @@ public class GitCaller {
         });
     }
 
+
+    /**
+     * Compute the statements modified by the commit
+     * @param diffEntry a {@link DiffEntry}
+     * @param edit an {@link Edit}
+     */
     private void manageReplace(DiffEntry diffEntry, Edit edit) {
-        int before=  edit.getEndA() - edit.getBeginA();
-        int after =  edit.getEndB() - edit.getBeginB();
 
-        if (before > after) {
-            //lines updated AND deleted
-            LOGGER.fine("lines updated and deleted. Computing the statement diff");
+        BlockStmt blockStmtBefore = getStatementsFromOldPath(diffEntry, edit);
+        BlockStmt blockStmtAfter = getStatementsFromNewPath(diffEntry, edit);
 
-        }
+        LOGGER.fine(blockStmtBefore.getStatements().size()+ " statements before and "+blockStmtAfter.getStatements().size()+" after. Now comparing.");
 
-        if (after > before) {
-            //lines updated AND added
-            LOGGER.fine("lines updated and added. Computing the statement diff");
-        }
-
-        if (after == before) {
-            //lines updated
-            LOGGER.fine("lines updated. Computing the statement diff");
-
-            compareEditsSameSize(diffEntry, edit);
-        }
-    }
-
-    private void compareEditsSameSize(DiffEntry diffEntry, Edit edit) {
-        for (AtomicInteger line = new AtomicInteger(edit.getBeginA()); line.get() <= edit.getEndA(); line.incrementAndGet()) {
-
-            String sourceLine = null;
-            String targetLine = null;
-            try {
-                sourceLine = DiffUtils.getLineFromFile(DiffUtils.getFileContentFromCommit(repository, oldTree, diffEntry.getOldPath()), line.get());
-                targetLine = DiffUtils.getLineFromFile(DiffUtils.getFileContentFromCommit(repository, newTree, diffEntry.getNewPath()), edit.getBeginB());
-            } catch (IOException e) {
-                LOGGER.log(Level.WARNING, "Couldn't get the line from the old file", e);
+        blockStmtBefore.getStatements().forEach(statement -> {
+            if (! blockStmtAfter.getStatements().contains(statement) && statement.getRange().isPresent()) {
+                LOGGER.info(statement+
+                        " file: "+
+                        diffEntry.getOldPath()+
+                        " line: "+statement.getRange().get().begin.line+
+                        " from "+statement.getRange().get().begin.column+
+                        " to "+statement.getRange().get().end.column+
+                        " is changed");
             }
+        });
 
-            //computing the statements present in that line
-            BlockStmt blockStmtSrc = JavaParser.parseBlock("{" + sourceLine + "}");
-            BlockStmt blockStmtTrgt = JavaParser.parseBlock("{" + targetLine + "}");
+        blockStmtAfter.getStatements().forEach(statement -> {
+            if (! blockStmtBefore.getStatements().contains(statement) && statement.getRange().isPresent()) {
+                LOGGER.info(statement+
+                        " file: "+
+                        diffEntry.getNewPath()+
+                        " line: "+statement.getRange().get().begin.line+
+                        " from "+statement.getRange().get().begin.column+
+                        " to "+statement.getRange().get().end.column+
+                        " is new");
+            }
+        });
 
-        }
     }
+
 
     /**
      * Iterate over the statements deleted in the old compilation unit and get the coordinates, in order to find them in the impact analysis model
@@ -193,4 +205,73 @@ public class GitCaller {
         }
     }
 
+    /**
+     * Parse a file to gather all the statements from a specific {@link DiffEntry}, form the old state of the code
+     * @param diffEntry a {@link DiffEntry}
+     * @param edit an {@link Edit}
+     * @return a {@link BlockStmt}
+     */
+    private BlockStmt getStatementsFromOldPath(DiffEntry diffEntry, Edit edit) {
+        BlockStmt blockStmtBefore = new BlockStmt();
+        for (AtomicInteger i = new AtomicInteger(edit.getBeginA()); i.get() < edit.getEndA(); i.incrementAndGet()) {
+
+            String line = null;
+            try {
+                line = DiffUtils.getLineFromFile(DiffUtils.getFileContentFromCommit(repository, oldTree, diffEntry.getOldPath()), i.get());
+            } catch (IOException e) {
+                LOGGER.log(Level.FINE, "Couldn't get the statements of the following line: "+line, e);
+            }
+
+            try {
+                BlockStmt blockStmt = JavaParser.parseBlock("{"+line+"}");
+
+                blockStmt.getStatements().forEach(statement -> {
+                    Position begin = new Position(i.get(), statement.getBegin().get().column);
+                    Position end = new Position(i.get(), statement.getEnd().get().column);
+                    statement.setRange(new Range(begin, end));
+                });
+
+                blockStmtBefore.getStatements().addAll(blockStmt.getStatements());
+            } catch (ParseProblemException e) {
+                LOGGER.log(Level.FINE, "Couldn't get the statements of the following line: "+line, e);
+            }
+        }
+
+        return blockStmtBefore;
+    }
+
+    /**
+     * Parse a file to gather all the statements from a specific {@link DiffEntry}, form the new state of the code
+     * @param diffEntry a {@link DiffEntry}
+     * @param edit an {@link Edit}
+     * @return a {@link BlockStmt} containing the statements
+     */
+    private BlockStmt getStatementsFromNewPath(DiffEntry diffEntry, Edit edit) {
+        BlockStmt blockStmtAfter = new BlockStmt();
+        for (AtomicInteger i = new AtomicInteger(edit.getBeginB()); i.get() < edit.getEndB(); i.incrementAndGet()) {
+            String line = null;
+
+            try {
+                line = DiffUtils.getLineFromFile(DiffUtils.getFileContentFromCommit(repository, newTree, diffEntry.getNewPath()), i.get());
+            } catch (IOException e) {
+                LOGGER.log(Level.FINE, "Couldn't get the following line: "+i, e);
+            }
+
+            try {
+                BlockStmt blockStmt = JavaParser.parseBlock("{"+line+"}");
+
+                blockStmt.getStatements().forEach(statement -> {
+                    Position begin = new Position(i.get(), statement.getBegin().get().column);
+                    Position end = new Position(i.get(), statement.getEnd().get().column);
+                    statement.setRange(new Range(begin, end));
+                });
+
+                blockStmtAfter.getStatements().addAll(blockStmt.getStatements());
+            } catch (ParseProblemException e) {
+                LOGGER.log(Level.FINE, "Couldn't get the statements of the following line: "+line, e);
+            }
+        }
+
+        return blockStmtAfter;
+    }
 }
--- src/test/java/com/tblf/diff/GitCallerTest.java
+++ src/test/java/com/tblf/diff/GitCallerTest.java
@@ -1,10 +1,17 @@
 package com.tblf.diff;
 
+import com.tblf.business.Manager;
+import com.tblf.util.Configuration;
+import com.tblf.util.ModelUtils;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -25,7 +32,39 @@ public class GitCallerTest {
     public void checkCompareCommit() {
         File file = new File("src/test/resources/files");
         Assert.assertTrue(file.exists());
-        GitCaller gitCaller = new GitCaller(file);
+        GitCaller gitCaller = new GitCaller(file, new ResourceSetImpl());
         gitCaller.compareCommits("HEAD~1");
+    }
+
+    @Test
+    public void checkCompareCommitStandardProject() throws IOException {
+        File zip = new File("src/test/resources/fullProject/SimpleProject.zip");
+        ModelUtils.unzip(zip);
+
+        Configuration.setProperty("mode", "SOURCECODE");
+        Manager manager = new Manager();
+
+        File file = new File("src/test/resources/fullProject/SimpleProject");
+        try {
+            long before = System.currentTimeMillis();
+            File trace = manager.buildTraces(file);
+
+            System.out.println("Elapsed time: " + String.valueOf(System.currentTimeMillis() - before)+" ms");
+
+            Assert.assertNotNull(trace);
+
+            Resource analysis = manager.parseTraces(trace);
+
+            Assert.assertNotNull(analysis);
+            analysis.getResourceSet().getResources().forEach(resource -> System.out.println(resource.getURI()));
+
+            GitCaller gitCaller = new GitCaller(file, analysis.getResourceSet());
+            gitCaller.compareCommits("HEAD~1");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+
+        FileUtils.deleteDirectory(file);
     }
 }
