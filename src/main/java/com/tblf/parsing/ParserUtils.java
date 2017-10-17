98bf877602cabf5c204a6883c694711287a917a1
orichalque <thiblf@gmail.com>
2017-10-17 16:58:42 +0200

    util method added to get the Package model using the pckage qualified name

--- src/main/java/com/tblf/parsing/ParserUtils.java
+++ src/main/java/com/tblf/parsing/ParserUtils.java
@@ -1,5 +1,13 @@
 package com.tblf.parsing;
 
+import com.tblf.util.Configuration;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by Thibault on 02/10/2017.
  */
@@ -13,5 +21,30 @@ public class ParserUtils {
     public static String getPackageQNFromClassQN(String classQualifiedName) {
         String[] split = classQualifiedName.split("\\.");
         return classQualifiedName.replace(".".concat(split[split.length-1]), "");
+    }
+
+    /**
+     * Parse the URI of a Class file and return the package QN of its container. Uses the {@link Configuration} file
+     * @param file a {@link File} such as "src/main/java/com/tblf/SimpleProject/App.java"
+     * @return the package qualified name (e.g.) "com.tblf.SimpleProject"
+     */
+    public static String getPackageQNFromSUTFile(File file){
+        String sutUri = Configuration.getProperty("sut");
+        String filePath = file.getPath();
+
+        String[] path = filePath.split("/");
+        String[] sut = sutUri.split("/");
+
+        List<String> pathList = new LinkedList<>(Arrays.asList(path));
+
+        pathList.remove(pathList.size()-1);
+        for (String repo : sut) {
+            int item = pathList.indexOf(repo);
+            if (item != -1) {
+                pathList.remove(item);
+            }
+        }
+
+        return pathList.stream().collect(Collectors.joining("."));
     }
 }
--- src/test/java/com/tblf/parsing/ParserUtilsTest.java
+++ src/test/java/com/tblf/parsing/ParserUtilsTest.java
@@ -3,6 +3,8 @@ package com.tblf.parsing;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Created by Thibault on 02/10/2017.
  */
@@ -12,5 +14,13 @@ public class ParserUtilsTest {
     public void checkGetPackageQNFromClassQN() {
         String classQN = "com.tblf.package.MyClass";
         Assert.assertEquals("com.tblf.package", ParserUtils.getPackageQNFromClassQN(classQN));
+    }
+
+    @Test
+    public void checkGetPackageQNFromSUTClassUri() {
+        File file = new File("src/main/java/com/tblf/SimpleProject/App.java");
+
+        String packageQN = ParserUtils.getPackageQNFromSUTFile(file);
+        Assert.assertEquals("com.tblf.SimpleProject", packageQN);
     }
 }
