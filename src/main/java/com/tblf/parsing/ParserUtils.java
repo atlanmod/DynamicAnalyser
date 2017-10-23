81e73a80d89eec0a4a6585b15e4595d69e01a745
orichalque <thiblf@gmail.com>
2017-10-23 17:16:45 +0200

    Corrected the method to find the Java2Directory out of a package name

--- src/main/java/com/tblf/parsing/ParserUtils.java
+++ src/main/java/com/tblf/parsing/ParserUtils.java
@@ -30,12 +30,37 @@ public class ParserUtils {
     }
 
     /**
-     * Parse the URI of a Class file and return the package QN of its container. Uses the {@link Configuration} file
+     * Parse the URI of a SUT Class file and return the package QN of its container. Uses the {@link Configuration} file
      * @param file a {@link File} such as "src/main/java/com/tblf/SimpleProject/App.java"
      * @return the package qualified name (e.g.) "com.tblf.SimpleProject"
      */
     public static String getPackageQNFromSUTFile(File file){
         String sutUri = Configuration.getProperty("sut");
+        String filePath = file.getPath();
+
+        String[] path = filePath.split("/");
+        String[] sut = sutUri.split("/");
+
+        List<String> pathList = new LinkedList<>(Arrays.asList(path));
+
+        pathList.remove(pathList.size()-1);
+        for (String repo : sut) {
+            int item = pathList.indexOf(repo);
+            if (item != -1) {
+                pathList.remove(item);
+            }
+        }
+
+        return pathList.stream().collect(Collectors.joining("."));
+    }
+
+    /**
+     * Parse the URI of a test Class file and return the package QN of its container. Uses the {@link Configuration} file
+     * @param file a {@link File} such as "src/main/java/com/tblf/SimpleProject/App.java"
+     * @return the package qualified name (e.g.) "com.tblf.SimpleProject"
+     */
+    public static String getPackageQNFromTestFile(File file){
+        String sutUri = Configuration.getProperty("test");
         String filePath = file.getPath();
 
         String[] path = filePath.split("/");
