08f8655c1b4ad49233c9786016a5f856c0ccb6f4
orichalque <thiblf@gmail.com>
2017-10-25 11:28:16 +0200

    Changed the class. Now building JUnit Requests and running it, and logging the results instead of running Class<> roughly

--- src/main/java/com/tblf/runner/JUnitRunner.java
+++ src/main/java/com/tblf/runner/JUnitRunner.java
@@ -1,23 +1,25 @@
 package com.tblf.runner;
 
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
 import org.junit.runner.Result;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Created by Thibault on 19/09/2017.
  */
 public class JUnitRunner {
-    private ClassLoader classLoader;
+
     private static final Logger LOGGER = Logger.getLogger("JUnitRunner");
-    private int success;
-    private int failure;
-    private int ignore;
-    public JUnitRunner() {
-        classLoader = ClassLoader.getSystemClassLoader();
-    }
+    private static final JUnitCore J_UNIT_CORE = new JUnitCore();
+
+    private ClassLoader classLoader;
 
     public JUnitRunner(ClassLoader classLoader) {
         this.classLoader = classLoader;
@@ -25,28 +27,76 @@ public class JUnitRunner {
 
     /**
      * Run the tests of a given JUnit test list after loading them in the System {@link ClassLoader}
-     * @param tests
+     *
+     * @param tests a {@link Collection} of test class names
      */
     public void runTests(Collection<String> tests) {
+        LOGGER.info("Running " + tests.size() + " test suites");
 
-        LOGGER.info("Running "+tests.size()+" test suites");
-        JUnitCore jUnitCore = new JUnitCore();
-        success = 0;
-        failure = 0;
-        ignore = 0;
-        tests.forEach(s -> {
+        Collection<Request> requests = tests.stream().map(s -> {
+            Request request = null;
             try {
-                LOGGER.info("Running test "+s);
-                Result result = jUnitCore.run(classLoader.loadClass(s));
-                failure += result.getFailureCount();
-                success += result.getRunCount() - result.getFailureCount();
-                ignore += result.getIgnoreCount();
-                LOGGER.info(RunnerUtils.results(result));
-            } catch (Throwable e) {
-                LOGGER.warning("Couldn't run the tests of class: "+s+" : "+e.getMessage());
+                request = Request.aClass(classLoader.loadClass(s));
+            } catch (ClassNotFoundException e) {
+                LOGGER.log(Level.WARNING, "couldn't load the test class " + s, e);
             }
+            return request;
+        }).collect(Collectors.toList());
+
+        this.analyseResults(this.runRequests(requests));
+    }
+
+    /**
+     * Run all the method in the @{@link Map.Entry} Collection. (MethodName, ClassName)
+     *
+     * @param entries a collection of Classes and methods
+     */
+    public void runTestMethods(Collection<Map.Entry<String, String>> entries) {
+        LOGGER.info("Running" + entries.size() + " test methods");
+
+        Collection<Request> requests = entries.stream().map(entry -> {
+            Request request = null;
+            try {
+                Class aClass = classLoader.loadClass(entry.getValue());
+                request = Request.method(aClass, entry.getKey());
+            } catch (ClassNotFoundException e) {
+                LOGGER.log(Level.WARNING, "couldn't load the test class " + entry.getValue(), e);
+            }
+            return request;
+        }).collect(Collectors.toList());
+
+        this.analyseResults(this.runRequests(requests));
+    }
+
+    /**
+     * Run a collection of {@link Request} and returns a collection of {@link Result}
+     *
+     * @param requests a collection of {@link Request}
+     * @return a collection of {@link Result}
+     */
+    private Collection<Result> runRequests(Collection<Request> requests) {
+        return requests
+                .stream()
+                .map(J_UNIT_CORE::run)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Iterate over a collection of {@link Result} and log the overall results
+     * @param results a {@link Collection} of {@link Result}
+     */
+    private void analyseResults(Collection<Result> results) {
+        AtomicInteger failure = new AtomicInteger(0);
+        AtomicInteger success = new AtomicInteger(0);
+        AtomicInteger ignore = new AtomicInteger(0);
+
+        results.forEach(result -> {
+            failure.addAndGet(result.getFailureCount());
+            success.addAndGet(result.getRunCount() - result.getFailureCount());
+            ignore.addAndGet(result.getIgnoreCount());
+            LOGGER.fine(RunnerUtils.results(result));
         });
 
-        LOGGER.info((failure + success) + " tests run : "+success+ " succeeded - "+failure+" failed - "+ignore+" ignored");
+        LOGGER.info((failure.get() + success.get()) + " tests run : " + success.get() + " succeeded - " + failure.get() + " failed - " + ignore.get() + " ignored");
     }
 }
