da9715822629cb90c13d6792d2044271cd4c700e
Thibault Béziers la Fosse <thiblf@gmail.com>
2018-06-28 11:48:47 +0200

    Created a new tracer using a MQTT client to publish traces

---
+++ instrumentation/src/main/java/com/tblf/linker/tracers/MqttTracer.java
@@ -0,0 +1,90 @@
+package com.tblf.linker.tracers;
+
+import com.tblf.utils.Configuration;
+import org.fusesource.mqtt.client.*;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MqttTracer implements Tracer{
+    private static final Logger LOGGER = Logger.getLogger("MqttTracer");
+    private BlockingConnection blockingConnection;
+
+    public MqttTracer() {
+        MQTT mqtt = new MQTT();
+        try {
+            mqtt.setHost(Configuration.getProperty("mqttHost"), Integer.parseInt(Configuration.getProperty("mqttPort")));
+            blockingConnection = mqtt.blockingConnection();
+            blockingConnection.connect();
+        } catch (URISyntaxException e) {
+            LOGGER.log(Level.WARNING, "Could not initialize MQTT connection", e);
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Could not open a blocking connection", e);
+        }
+    }
+
+    @Override
+    public void write(String value) {
+        try {
+            blockingConnection.publish("trace", value.getBytes(), QoS.AT_MOST_ONCE, false);
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Could not publish", e);
+        }
+    }
+
+    @Override
+    public void write(String topic, String value) {
+        try {
+            blockingConnection.publish(topic, value.getBytes(), QoS.AT_MOST_ONCE, false);
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Could not publish", e);
+        }
+    }
+
+    @Override
+    public void updateTest(String className, String methodName) {
+
+    }
+
+    @Override
+    public void updateTarget(String className, String methodName) {
+
+    }
+
+    @Override
+    public void updateStatementsUsingColumn(String startPos, String endPos) {
+
+    }
+
+    @Override
+    public void updateStatementsUsingLine(String line) {
+
+    }
+
+    @Override
+    public void startTrace() {
+
+    }
+
+
+    @Override
+    public void endTrace() {
+        try {
+            blockingConnection.disconnect();
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Could not close the connection", e);
+        }
+    }
+
+    @Override
+    public File getFile() {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
---
+++ instrumentation/src/test/java/com/tblf/linker/MqttTracerTest.java
@@ -0,0 +1,60 @@
+package com.tblf.linker;
+
+import com.tblf.linker.tracers.MqttTracer;
+import com.tblf.linker.tracers.Tracer;
+import com.tblf.utils.Configuration;
+import io.moquette.server.Server;
+import org.fusesource.mqtt.client.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class MqttTracerTest {
+    private Server server;
+    private Tracer tracer;
+
+    @Before
+    public void setUp() throws IOException {
+        Configuration.setProperty("mqttHost", "0.0.0.0");
+        Configuration.setProperty("mqttPort", "1883");
+        File conf = new File("src/test/resources/moquette.conf");
+        server = new Server();
+        server.startServer(conf);
+    }
+
+    @Test
+    public void newMqttTracer() throws IOException {
+        tracer = new MqttTracer();
+    }
+
+    @Test
+    public void writeMqttTracer() throws Exception {
+
+        tracer = new MqttTracer();
+
+        MQTT mqtt = new MQTT();
+        mqtt.setHost("0.0.0.0", 1883);
+        BlockingConnection blockingConnection = mqtt.blockingConnection();
+        blockingConnection.connect();
+        blockingConnection.subscribe(new Topic[]{new Topic("trace", QoS.AT_MOST_ONCE)});
+
+        tracer.write("testValue");
+        Message message =  blockingConnection.receive();
+        tracer.endTrace();
+
+        String content = new String(message.getPayload(), Charset.defaultCharset());
+
+        Assert.assertEquals("not the expected value", "testValue", content);
+    }
+
+    @After
+    public void tearDown() {
+        tracer.endTrace();
+        server.stopServer();
+    }
+}
